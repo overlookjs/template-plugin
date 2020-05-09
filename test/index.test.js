75b3ab77252cea386d7f75a6c849d0146fad99cf
overlookmotel <theoverlookmotel@gmail.com>
2020-05-10 00:03:11 +0100

    Tests: Test plugin subclasses Route

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@
 
 // Modules
 const Plugin = require('@overlook/plugin'),
+	Route = require('@overlook/route'),
 	{{name}}Plugin = require('@overlook/plugin-{{name}}');
 
 // Init
@@ -17,5 +18,13 @@ require('./support/index.js');
 describe('Plugin', () => { // eslint-disable-line jest/lowercase-name
 	it('is an instance of Plugin class', () => {
 		expect({{name}}Plugin).toBeInstanceOf(Plugin);
+	});
+
+	it('when passed to `Route.extend()`, returns subclass of Route', () => {
+		// TODO Capitalize class name below
+		const {{name}}Route = Route.extend({{name}}Plugin);
+		expect({{name}}Route).toBeFunction();
+		expect(Object.getPrototypeOf({{name}}Route)).toBe(Route);
+		expect(Object.getPrototypeOf({{name}}Route.prototype)).toBe(Route.prototype);
 	});
 });
